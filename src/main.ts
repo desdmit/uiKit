a7c294928f4c90469ba48490fbd5cf2ee40289c9
desdmit <desdmit@gmail.com>
2019-03-26 15:13:27 +0300

    fixed major issues. moved and cleaned code.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import './polyfills';
 
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { UiKitModule } from './app/ui-kit.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+platformBrowserDynamic()
+  .bootstrapModule(UiKitModule)
+  .then(ref => {
+    // Ensure Angular destroys itself on hot reloads.
+    if (window[`ngRef`]) {
+      window[`ngRef`].destroy();
+    }
+    window[`ngRef`] = ref;
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+    // Otherwise, log the boot error
+  })
+  .catch(err => window.console.error(err));
